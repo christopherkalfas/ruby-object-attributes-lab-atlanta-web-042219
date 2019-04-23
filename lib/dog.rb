30ba4814c7ea00d98455aa9034ebb8ff61a9bb64
github username <github email address>
2019-04-23 18:19:52 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+    @name = name
+
+
+def name
+  @name
+end
+
+def name=(new_name)
+  @name = new_name
+end
+breed = @breed
+def breed
+  @breed
+end
+
+def breed=(new_breed)
+  @breed = new_breed
+end
+
+end
